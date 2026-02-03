4ed86dc77ea432df92e4ea14bc4e8ff2d3554670
sheikazaruddin-oss <sheikmohammedazaruddin.sheiknizamuddin@sjsu.edu>
2026-02-03 11:32:10 -0800

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,7 @@ def show_logo_top_left(image_path, width=120):
     )
 
 show_logo_top_left("logo.png")  # Call the logo
+st.markdown("<br><br><br><br><br>", unsafe_allow_html=True)
 st.set_page_config(page_title="Conflict Plan Generator")
 st.title("✈️ Conflict Plan Generator")
 st.subheader("Ownership Aircraft Parameters")
